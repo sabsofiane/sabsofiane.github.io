2c18d210852cdde42c740fbc3e14e610a34cac5f
sabsofiane <sabsofiane80@gmail.com>
2020-03-10 09:24:43 +0100

    update french radio stations

--- dzd/podcasts/setup.sh
+++ dzd/podcasts/setup.sh
@@ -1,1 +1,1 @@
-curl -L -O -C - https://podcast.radioalgerie.dz/sources/Chaine3/mp3/bd4db7cb-8465-40c5-a0f7-9326ea146e69.mp3
+curl -L -O -C - https://podcast.radioalgerie.dz/sources/Chaine3/mp3/026efebf-88fd-469f-9251-2bb5520e7b76.mp3
