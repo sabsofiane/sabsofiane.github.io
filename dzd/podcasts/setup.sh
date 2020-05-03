b3d6acf16dc774f6ea3269c845016d46960650f9
sabsofiane <sabsofiane80@gmail.com>
2020-05-03 14:23:32 +0200

    update podcasts

--- dzd/podcasts/setup.sh
+++ dzd/podcasts/setup.sh
@@ -1,1 +1,1 @@
-curl -L -O -C - https://podcast.radioalgerie.dz/sources/Chaine3/mp3/12194a7a-ede8-47a8-9fcf-986cd73d3ae5.mp3
+curl -L -O -C - https://podcast.radioalgerie.dz/sources/Chaine3/mp3/44e7342e-e3fd-473b-99cc-764200a53fff.mp3
